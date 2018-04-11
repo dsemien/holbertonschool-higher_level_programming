abf7cea5f61e95515972a7b976552e34cecbdbd1
dusan <dsemien2@gmail.com>
2018-04-11 01:21:03 +0000

    changed if

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 9):
     for digit in range(number + 1, 10):
-        if number != digit:
+        if number < 8 or digit < 9:
             print("{:d}{:d}".format(number, digit),end=", ")
         else:
             print("{:d}{:d}".format(number, digit))
