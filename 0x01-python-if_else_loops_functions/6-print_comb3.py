0d012253eb38fc6eb07e739e1bd3f2a426451573
dusan <dsemien2@gmail.com>
2018-04-11 01:22:14 +0000

    pep error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for number in range(0, 9):
     for digit in range(number + 1, 10):
         if number < 8 or digit < 9:
-            print("{:d}{:d}".format(number, digit),end=", ")
+            print("{:d}{:d}".format(number, digit), end=", ")
         else:
             print("{:d}{:d}".format(number, digit))
