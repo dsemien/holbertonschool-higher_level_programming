08728002bb67c012611c4022e530030452eeb925
dusan <dsemien2@gmail.com>
2018-04-13 01:25:02 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    pirate = len(argv) - 1 
+    index = 1 
+    if len(argv) == 1:
+        print("{} arguments.".format(pirate))
+    elif len(argv) == 2:
+        print("{} argument:".format(pirate))
+        print("{}: {}".format(index, argv[index]))
+    else:
+        print("{} arguments:".format(pirate))
+        for index in range(1, len(argv)):
+            print("{}: {}".format(index, argv[index]))
