ac31caff7164c0fb1eedb40136ab70bea31fe6db
dusan <dsemien2@gmail.com>
2018-04-17 23:17:46 +0000

    format specifiyer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for number in range(len(matrix)):
         for pos in range(len(matrix[number])):
             if pos == len(matrix[number]) - 1:
-                print("{}".format(matrix[number][pos]), end="")
+                print("{:d}".format(matrix[number][pos]), end="")
             else:
-                print("{}".format(matrix[number][pos]), end=" ")
+                print("{:d}".format(matrix[number][pos]), end=" ")
         print()
