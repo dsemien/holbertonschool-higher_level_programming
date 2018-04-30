6b2815f5e0a952a4964fe1f2cb7d2e980f53fdf2
dusan <dsemien2@gmail.com>
2018-04-30 23:52:19 +0000

    remove space

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     idx = 0
     for element in range(x):
         try:
-            print("{:d}" .format(my_list[element]), end="")
+            print("{:d}".format(my_list[element]), end="")
             idx += 1
         except (ValueError, TypeError):
             pass
