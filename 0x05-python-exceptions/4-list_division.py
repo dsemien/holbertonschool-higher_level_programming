6e9effaa8896bdcb8f0497d7c7c9403f967f5a4e
dusan <dsemien2@gmail.com>
2018-05-01 00:44:59 +0000

    pep error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for digit in range(list_length):
         try:
             div = my_list_1[digit] / my_list_2[digit]
-            
+
         except TypeError:
             print("wrong type")
             div = 0
@@ -20,5 +20,5 @@ def list_division(my_list_1, my_list_2, list_length):
 
         finally:
             new_list.append(div)
-            
+
     return(new_list)
