092102fe719ae1666e69adeb0db3ad4a44f91f66
dusan <dsemien2@gmail.com>
2018-05-02 04:17:04 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,29 +33,30 @@ class Square:
         return(self.__size)
 
     @size.setter  # setter
-    def size(self, resize):
+    def size(self, pos):
         """ Initialization of set __size attribute
         Arguments:
             resize (int): new size of square
         """
-        if isinstance(resize, int) is not True:  # check if var is int
+        if isinstance(pos, int) is not True:  # check if var is int
             raise TypeError("size must be an integer")
-        if resize < 0:
+        if pos < 0:
             raise ValueError("size must be >= 0")
-        self.__size = resize
+        self.__size = pos
 
     def my_print(self):
         """ prints square to console using #
         """
-        if self.__size is 0:
+        if self.size is 0:
             print()
         else:
             vert, horzon = self.position
-            for space in range(horzon):
+            for sqace in range(horzon):
                 print()
             for sqr in range(self.size):    
                 print(" " * vert, end="")
                 print("#" * self.size)
+
 
     @property  # getter
     def position(self):
@@ -71,11 +72,12 @@ class Square:
         Arguments:
             pos (int): position of square
         """
+        vert, horzon = pos
         if isinstance(pos, tuple) is not True:  # check if var is tuple
             raise TypeError("position must be a tuple of 2 positive integers")
         if isinstance(vert, int) is not True or isinstance(horzon, int) is not True:  # check if var is int
             raise TypeError("position must be a tuple of 2 positive integers")
         if vert < 0 or horzon < 0 or len(pos) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        vert, horzon = pos
-        self.__position = pos
+        else:
+            self.__position = pos
