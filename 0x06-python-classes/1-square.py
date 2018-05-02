30855cb2eaabd32fe199bd02990627bf4dbb822f
dusan <dsemien2@gmail.com>
2018-05-02 00:01:39 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Creation of a Class
+"""
+
+
+class Square:
+    """ fancy square
+    Attributes:
+        size (str): size of square
+    """
+    def __init__(self, size):
+        """ Initialization of square
+        Arguments:
+            size (str): size of square
+        """
+        self.__size = size
