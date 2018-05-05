64cacb61208433933dafdcb4ff564ea38ef635c5
dusan <dsemien2@gmail.com>
2018-05-05 22:19:28 +0000

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""progaram that says your name!
+"""
+def say_my_name(first_name, last_name=""):
+    """A progaram that says your name!
+
+    Arguments:
+        first_name (str): "First Name"
+        last_name (str): "Second Name"
+
+    Raises:
+        TypeError: if first_name or last_name is not a str
+    """
+    if isinstance(first_name, str) is not True:
+            raise TypeError("first_name must be a string")
+    if isinstance(last_name, str) is not True:
+            raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
