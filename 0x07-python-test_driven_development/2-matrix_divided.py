b096ea1e714d26ecd504172eaecade8e9404cf21
dusan <dsemien2@gmail.com>
2018-05-06 03:13:08 +0000

    pep errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""progaram that divides all 
+"""progaram that divides all
     elements of a matrix.
 """
 
+
 def matrix_divided(matrix, div):
     """a function that divides all elements of a matrix.
-    
+
     Arguments:
         matirx (list): matrix of integers.
         div (int): number divisable against matrix.
@@ -18,11 +19,14 @@ def matrix_divided(matrix, div):
         result of divisable matrix
     """
     if isinstance(matrix, list) is not True:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            "of integers/floats")
     if isinstance(matrix[0], list) is not True:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            "of integers/floats")
     if isinstance(matrix[1], list) is not True:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            "of integers/floats")
     if len(matrix[0]) is not len(matrix[1]):
             raise TypeError("Each row of the matrix must have the same size")
     if isinstance(div, int) is not True:
