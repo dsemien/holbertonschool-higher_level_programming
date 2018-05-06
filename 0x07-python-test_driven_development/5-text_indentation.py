6bbd7f8498f19370feef72b8b2b01f8aa9f34a31
dusan <dsemien2@gmail.com>
2018-05-06 03:40:55 +0000

    pep error

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""A progaram  that prints a text with 
+"""A progaram  that prints a text with
 2 new lines after each of these characters: ., ? and :
 """
+
+
 def text_indentation(text):
-    """A function that prints a text with 
+    """A function that prints a text with
     2 new lines after each of these characters: ., ? and:
 
     Arguments:
             text (str): String of text to print
 
         Raises:
-            TypeError: if text in not an str.        
+            TypeError: if text in not an str.
     """
     delim = {'.', '?', ':'}
     if isinstance(text, str) is not True:
@@ -20,6 +22,8 @@ def text_indentation(text):
         if letters in delim:
             print("{:s}\n".format(letters))
         else:
-            if (doc[page - 1] in delim or (doc[page] is " " and doc[page -1] is " ")):
+            if doc[page - 1] in delim or doc[page] is " ":
+                continue
+            if doc[page - 1] is " ":
                 continue
             print("{:s}".format(letters), end="")
