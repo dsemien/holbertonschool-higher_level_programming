c7e423f1056a915011b71aa46d0deb45c8550bd2
dusan <dsemien2@gmail.com>
2018-05-16 23:50:29 +0000

    Student to JSON

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,9 +16,9 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
-        """ A JSON methon that retrieves a 
+        """ A JSON methon that retrieves a
             dictionary representation of a Student instance
             Returns:
                 a dictionary representation of a Student instance
