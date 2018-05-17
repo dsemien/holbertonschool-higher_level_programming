5c5d2a9e7c80c6cb5fd552a603bbf5adaef95556
dusan <dsemien2@gmail.com>
2018-05-17 02:11:53 +0000

    fix output

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     with open(filename, encoding='UTF-8') as f:
-        for linecount, line in enumerate(f):
-            pass    
-        return linecount + 1
+        linecount = 0
+        for linecount, line in enumerate(f, 1):
+            pass
+        return linecount
